d0d290091aefc26eee9005e9ffccbbdf8e2689a7
Aaron <troublor@live.com>
2020-08-03 21:34:47 +0800

    no need to yarn build since this is already done in flash docker-all

--- packages/darcher-examples/augurproject/scripts/start_augur_all_docker.ts
+++ packages/darcher-examples/augurproject/scripts/start_augur_all_docker.ts
@@ -8,17 +8,13 @@ async function start_blockchain() {
     logger.info("Start blockchain cluster in docker");
     let cmd = new Command("docker-compose", "-f", path.join(__dirname, "docker", "docker-compose.yml"), "up", "-d");
     shell.exec(cmd.toString());
-    logger.info("Wait 3 seconds for blockchain to start...");
-    await sleep(3000);
 }
 
 async function start_augur() {
     let cmd1 = new Command("yarn", "flash", "docker-all", "--do-not-run-geth", "run:gsn");
-    let cmd2 = new Command("tsc", "build", "-w");
     let cmd3 = new Command("yarn", "ui", "dev");
     let window = new TerminalWindow(
         new Tab(cmd1, true, path.join(__dirname, "..", "augur")),
-        new Tab(cmd2, false, path.join(__dirname, "..", "augur")),
         new Tab(cmd3, false, path.join(__dirname, "..", "augur")));
     window.open();
 }
