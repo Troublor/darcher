bb827f82361c9c868a4db6a6e9d6dc3001ef92b1
Aaron <troublor@live.com>
2020-08-05 21:38:22 +0800

    kill geth when interrupt

--- packages/darcher-examples/giveth/scripts/deploy.ts
+++ packages/darcher-examples/giveth/scripts/deploy.ts
@@ -6,9 +6,27 @@ import * as path from "path";
 async function deployContracts(config: Config) {
     // reset clusters
     let cluster1 = new BlockchainCluster(config.clusters[0]);
+
+    async function kill1() {
+        console.log("stopping");
+        await cluster1.stop();
+    }
+
+    process.on("SIGINT", kill1);
+    process.on("SIGHUP", kill1);
+    process.on("SIGTERM", kill1);
     cluster1.reset();
     await cluster1.deploy(true);
     let cluster2 = new BlockchainCluster(config.clusters[1]);
+
+    async function kill2() {
+        console.log("stopping");
+        await cluster2.stop();
+    }
+
+    process.on("SIGINT", kill2);
+    process.on("SIGHUP", kill2);
+    process.on("SIGTERM", kill2);
     cluster2.reset();
     await cluster2.deploy(true);
 
