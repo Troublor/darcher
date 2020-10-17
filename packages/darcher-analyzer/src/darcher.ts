55f400114cfe75f1aa56463efaa84014aed67318
Aaron <troublor@live.com>
2020-10-17 20:01:17 +0800

    print debug logs when receiving and finishing "waitForTxProcess" grpc call

--- packages/darcher-analyzer/src/darcher.ts
+++ packages/darcher-analyzer/src/darcher.ts
@@ -120,7 +120,21 @@ export class Darcher {
         this.dappTestDriverHandler.onTestStart = analyzer.onTestStart.bind(analyzer);
         this.dappTestDriverHandler.onTestEnd = analyzer.onTestEnd.bind(analyzer);
         this.dappTestDriverHandler.onConsoleError = analyzer.onConsoleError.bind(analyzer);
-        this.dappTestDriverHandler.waitForTxProcess = analyzer.waitForTxProcess.bind(analyzer);
+        this.dappTestDriverHandler.waitForTxProcess = async msg1 => {
+            this.logger.debug(
+                `DApp waiting for Transaction process`,
+                {
+                    tx: prettifyHash(msg1.getHash()),
+                }
+            );
+            await analyzer.waitForTxProcess;
+            this.logger.debug(
+                `Transaction process complete, resume DApp`,
+                {
+                    tx: prettifyHash(msg1.getHash()),
+                }
+            );
+        };
     }
 
     private async selectTxToTraverse(msg: SelectTxControlMsg): Promise<string> {
