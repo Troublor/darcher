af7e95884bb574f694025d5a12ec9a36d8c3ccf1
Aaron <troublor@live.com>
2020-11-19 22:06:14 +0800

    delete metamask-extension submodule

--- packages/darcher-examples/MetaMask/metamask-extension/.storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: ['../ui/app/**/*.stories.js'],
-  addons: [
-    '@storybook/addon-knobs',
-    '@storybook/addon-actions',
-    '@storybook/addon-backgrounds'
-  ],
-}
