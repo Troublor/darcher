15e2791a2be74ae57e711e47e84e19e177b3b734
DArcher Framework <darcher-framework@outlook.com>
2021-02-21 19:48:07 +0800

    :rocket: build docker image for DemocracyEarth

--- packages/darcher-examples/DemocracyEarch/scripts/docker/build-image.sh
+++ packages/darcher-examples/DemocracyEarch/scripts/docker/build-image.sh
@@ -4,7 +4,7 @@ DIR=$( dirname "${BASH_SOURCE[0]}" )
 
 IMAGE_NAME=darcherframework/democracy-earth:latest
 
-docker build -t $IMAGE_NAME -f "$DIR"/Dockerfile "$DIR"/../..
+#docker build -t $IMAGE_NAME -f "$DIR"/Dockerfile "$DIR"/../..
 
 DAPP_IMAGE_NAME=darcherframework/democracy-earth-dapp:latest
 
